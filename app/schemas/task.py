3a5ff116e03fcd2cf354c2923ed87aa529277ef3
Mister D. Ace <ode808prod@gmail.com>
2025-09-20 11:47:29 +0200

    Implement User Schema

---
+++ app/schemas/task.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from typing import Optional
+from uuid import UUID
+from datetime import datetime
+
+
+class TaskBase(BaseModel):
+    pass
---
+++ app/schemas/user.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel, EmailStr
+from uuid import UUID
+from datetime import datetime
+from typing import Optional, List
+
+
+# Base schema (shared fields)
+class UserBase(BaseModel):
+
+    username: str
+    email: EmailStr
+
+
+# Schema for creating a user (requires password)
+class UserCreate(UserBase):
+
+    password: str
+
+
+# Schema for updating a user (all fields optional)
+class UserUpdate(BaseModel):
+
+    username: Optional[str] = None
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
+
+
+# Schema returned to the client (Read Model)
+class UserRead(UserBase):
+    id: UUID
+    created_at: datetime
+    updated_at: Optional[datetime]
+
+    class Config:
+        orm_mode = True  # allows reading from SQLAlchemy models
+
+
+# For including related tasks (if need)
+class UserWithTasks(UserRead):
+    # a forward ref, will be linked later
+    tasks: List[TaskRead] = []
